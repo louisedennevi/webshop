268c656a21d7a943632d2780275d129b8861d8c5
Louise Dennevi <louisedennevi@Louises-MBP.mediegruppen.se>
2020-02-19 11:19:04 +0100

    testing

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 // Härifrån startar vi upp vår webshop
-const {app, port} = require('./src/server')
+const {app, port, config} = require('./src/server')
+const mongoose = require('mongoose')
+
 
 
 // Klicka igång servern
-const server = app.listen(port, ()=> console.log(`App listening on port ${port}!`))
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}
+
+mongoose.connect(config.databaseURL, options).then(() => {
+    //app is listening to port 
+    app.listen(port, ()=> console.log(`App listening on port ${port}!`))
+})
+
+
 
 
--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 // Här ska vi definiera vår server
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 8080
+const config = require("../config/config")
 
 // Route till
 app.get('/', (req, res) => res.send('Hello World!'))
 
 
 
-module.exports = {app, port}
+
+
+module.exports = {app, port, config}
